ba4a7c202a0121ff9b71d0034b896fbdfa49e32e
Fan Ni <fan.ni@samsung.com>
2024-10-17 16:00:13 -0700

    ras.py: move package install upper level so packages can be shared

--- utils/ras.py
+++ utils/ras.py
@@ -24,7 +24,6 @@ def install_rasdaemon(url="https://github.com/moking/rasdaemon-clone", branch="s
     cmd="install rasdaemon..."
     print(cmd)
 
-    tools.install_packages_on_vm("git libtraceevent-dev libtraceevent1 pkg-config dh-autoreconf dmidecode")
     if tools.path_exist_on_vm(dire):
         choice=input("%s exists, delete it before git clone (Y/N): "%dire)
         if choice and choice.lower() == "y":
@@ -119,6 +118,7 @@ def install_aer_inject(url="https://git.kernel.org/pub/scm/linux/kernel/git/gong
 
 
 def install_ras_tools():
+    tools.install_packages_on_vm("git libtraceevent-dev libtraceevent1 pkg-config dh-autoreconf dmidecode flex")
     install_rasdaemon();
     install_mce_inject()
     install_mce_test()
