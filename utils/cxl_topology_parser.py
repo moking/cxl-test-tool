ef86b4ba21b2a7f428d4bbc7fca47499ad6188d0
Fan Ni <fan.ni@samsung.com>
2025-03-25 17:21:54 -0700

    add extra_opt attribute for endpoint dev for extra attr like share-mb and mb-share-init

--- utils/cxl_topology_parser.py
+++ utils/cxl_topology_parser.py
@@ -35,36 +35,45 @@ def create_cxl_rp(bus="cxl.1"):
     slot += 1
     return name, rs
 
-def create_cxl_pmem(parent_dport, size="512M"):
+def create_cxl_pmem(parent_dport, size="512M", extra_opt=""):
     global mem_id
     global serial
     hmem, hmem_str=create_object("hmem%s"%mem_id, size=size)
     lsa, lsa_str=create_object("lsa%s"%mem_id, size = "2M")
     name = "cxl-memdev%s"%mem_id
-    mem_str= "-device cxl-type3,bus=%s,memdev=%s,lsa=%s,id=cxl-memdev%s,sn=%s "%(parent_dport, hmem, lsa, mem_id, serial)
+    if extra_opt:
+        mem_str= "-device cxl-type3,bus=%s,memdev=%s,lsa=%s,id=cxl-memdev%s,sn=%s,%s "%(parent_dport, hmem, lsa, mem_id, serial, extra_opt)
+    else:
+        mem_str= "-device cxl-type3,bus=%s,memdev=%s,lsa=%s,id=cxl-memdev%s,sn=%s "%(parent_dport, hmem, lsa, mem_id, serial)
     mem_id += 1
     serial += 1
 
     return name, hmem_str+lsa_str+mem_str
 
-def create_cxl_vmem(parent_dport, size="512M"):
+def create_cxl_vmem(parent_dport, size="512M", extra_opt=""):
     global mem_id
     global serial
-    hmem, hmem_str=create_object("hmem%s"%mem_id, size=size)
+    hmem, hmem_str=create_object("hmem%s"%mem_id, size=size, extra_opt="")
     name = "cxl-memdev%s"%mem_id
-    mem_str= "-device cxl-type3,bus=%s,volatile-memdev=%s,id=cxl-vmemdev%s,sn=%s "%(parent_dport, hmem, mem_id, serial)
+    if extra_opt:
+        mem_str= "-device cxl-type3,bus=%s,volatile-memdev=%s,id=cxl-vmemdev%s,sn=%s,%s "%(parent_dport, hmem, mem_id, serial, extra_opt)
+    else:
+        mem_str= "-device cxl-type3,bus=%s,volatile-memdev=%s,id=cxl-vmemdev%s,sn=%s "%(parent_dport, hmem, mem_id, serial)
     mem_id += 1
     serial += 1
 
     return name, hmem_str+mem_str
 
-def create_cxl_mem(parent_dport, pmem=True, vmem=False, dcd=False):
+def create_cxl_mem(parent_dport, pmem=True, vmem=False, dcd=False, extra_opt=""):
     global mem_id
     global serial
     prefix= "-device cxl-type3,bus=%s,"%parent_dport
     lsa, lsa_str=create_object("lsa%s"%mem_id, size = "2M")
     name = "cxl-memdev%s"%mem_id
-    suffix="id=%s,sn=%s "%(name,serial)
+    if extra_opt:
+        suffix="id=%s,sn=%s,%s "%(name,serial, extra_opt)
+    else:
+        suffix="id=%s,sn=%s "%(name,serial)
     hmem_str=""
     vhmem_str=""
     dhmem_str=""
@@ -132,24 +141,25 @@ def parse_topo(root, p="", s=""):
     else:
         if root.tag == "rp":
             size="512M"
+            extra_opt =  root.attrib.get("extra_opt", "")
             if root.attrib.get("size"):
                 size = root.attrib["size"]
             name, rs = create_cxl_rp(p)
             s += rs
             if root.text =="pmem":
-                name, rs = create_cxl_pmem(name, size)
+                name, rs = create_cxl_pmem(name, size, extra_opt = extra_opt)
                 s += rs
             if root.text == "vmem" or root.text == "ram":
-                name, rs = create_cxl_vmem(name, size)
+                name, rs = create_cxl_vmem(name, size, extra_opt = extra_opt)
                 s += rs
             if root.text == "mixed":
-                name, rs = create_cxl_mem(name, pmem=True, vmem=True)
+                name, rs = create_cxl_mem(name, pmem=True, vmem=True, extra_opt = extra_opt)
                 s += rs
             if root.text == "mixed-dcd":
-                name, rs = create_cxl_mem(name, pmem=True, vmem=True, dcd=True)
+                name, rs = create_cxl_mem(name, pmem=True, vmem=True, dcd=True, extra_opt = extra_opt)
                 s += rs
             if root.text == "dcd":
-                name, rs = create_cxl_mem(name, pmem=False, vmem=False, dcd=True)
+                name, rs = create_cxl_mem(name, pmem=False, vmem=False, dcd=True, extra_opt = extra_opt)
                 s += rs
         elif root.tag == "switch":
             name, rs = create_cxl_switch(p)
@@ -157,24 +167,25 @@ def parse_topo(root, p="", s=""):
         elif root.tag == "dsp":
             dsid=root.attrib["id"]
             size="512M"
+            extra_opt =  root.attrib.get("extra_opt", "")
             if root.attrib.get("size"):
                 size = root.attrib["size"]
             name, rs = create_cxl_dsp(p, dsid)
             s += rs
             if root.text == "pmem":
-                name, rs = create_cxl_pmem(name, size)
+                name, rs = create_cxl_pmem(name, size, extra_opt = extra_opt)
                 s += rs
             if root.text == "vmem" or root.text == "ram":
-                name, rs = create_cxl_vmem(name, size)
+                name, rs = create_cxl_vmem(name, size, extra_opt = extra_opt)
                 s += rs
             if root.text == "mixed":
-                name, rs = create_cxl_mem(name, pmem=True, vmem=True)
+                name, rs = create_cxl_mem(name, pmem=True, vmem=True, extra_opt = extra_opt)
                 s += rs
             if root.text == "mixed-dcd":
-                name, rs = create_cxl_mem(name, pmem=True, vmem=True, dcd=True)
+                name, rs = create_cxl_mem(name, pmem=True, vmem=True, dcd=True, extra_opt = extra_opt)
                 s += rs
             if root.text == "dcd":
-                name, rs = create_cxl_mem(name, pmem=False, vmem=False, dcd=True)
+                name, rs = create_cxl_mem(name, pmem=False, vmem=False, dcd=True, extra_opt = extra_opt)
                 s += rs
         elif root.tag == "fmw":
             size = root.attrib.get("size", "4G")
